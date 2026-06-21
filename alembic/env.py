9f04b44de3b8098f9eedff0b00845e2d805334a7
Karmanovich Sergey <s.karmanovich@reg.ru>
2026-06-21 20:39:18 +0500

    BOOKING-8: Всегда использовать асинхронный алембик.

--- alembic/env.py
+++ alembic/env.py
@@ -1,19 +1,22 @@
+import asyncio
 import sys
 from logging.config import fileConfig
 from pathlib import Path
 
 from alembic import context
-from sqlalchemy import create_engine, pool
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
 
-from adapters.persistence.database import to_sync_database_url
+from adapters.persistence.database import create_db_engine
 from adapters.persistence.models import Base
 from adapters.settings import Settings
 
 config = context.config
 settings = Settings()
-config.set_main_option("sqlalchemy.url", to_sync_database_url(settings.database_url))
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -34,17 +37,24 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    connectable: AsyncEngine = create_db_engine(settings.database_url, poolclass=pool.NullPool)
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
 def run_migrations_online() -> None:
-    connectable = create_engine(
-        to_sync_database_url(settings.database_url),
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-
-        with context.begin_transaction():
-            context.run_migrations()
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
--- src/adapters/persistence/database.py
+++ src/adapters/persistence/database.py
@@ -16,12 +16,3 @@ def create_db_engine(
 
 engine = create_db_engine(settings.database_url)
 session_factory = async_sessionmaker(engine, expire_on_commit=False)
-
-
-# TODO:А разве нельзя всё делать на одном драйвере асинхронном с алхимией?
-def to_sync_database_url(database_url: str) -> str:
-    if database_url.startswith("sqlite+aiosqlite:"):
-        return "sqlite:" + database_url.removeprefix("sqlite+aiosqlite:")
-    if database_url.startswith("postgresql+asyncpg:"):
-        return database_url.replace("postgresql+asyncpg:", "postgresql+psycopg2:", 1)
-    return database_url
